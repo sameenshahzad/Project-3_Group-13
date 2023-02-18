32210cfea36591f09912b7cc1131dc505441b2dd
MTanguin <magtand@gmail.com>
2023-02-18 01:01:20 -0500

    Added ETL and modification on poject proposal

--- static/js/aq_logic.js
+++ static/js/aq_logic.js
@@ -1,22 +1,6 @@
 // air quality
-// health impacts
-// https://api-docs.iqair.com/?version=latest
-// Version 2
+
+// Load data 
 
 
-// List supported cities in a state
-// Fetch the data directly using a single line of command/code
-const apiKey = "{{56451c7f-da58-4196-8e40-576453cc9eb2}}";
-const url = `http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=${apiKey}`;
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    // Use the data here
-    console.log(data);
-  })
-  .catch(error => {
-    console.error("Error fetching data:", error);
-  });
-
- 
+  
